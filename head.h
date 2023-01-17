12471250b654fee99c8559f0d8b36a9ae100f225
Arif Rahman <119587470+Wisezx@users.noreply.github.com>
2023-01-17 19:07:00 +0700

    Add files via upload

---
+++ head.h
@@ -0,0 +1,18 @@
+#include <conio.h>
+#include <iostream.h>
+
+head(char jud[50]=""){
+	cout<<">-------------------------------------------------------------<"<<endl;
+   cout<<".....................::PASSWORDS SYARIAH::....................."<<endl;
+	cout<<"                       "<<jud<<endl;
+  	cout<<"---------------------------------------------------------------"<<endl;
+   if(strcmp(jud,"LOGIN APLIKASI")==0){
+    	cout<<"JUDUL     : PROGRAM PASSWORD"<<endl;
+   	cout<<"PEMBUAT   : ARIF RAHMAN"<<endl;
+   	cout<<"KELAS     : 10-PPLG"<<endl;
+      cout<<"KELOMPOK  : 9"<<endl;
+   	cout<<"VERSI APP : 5.2 (BUILD FULL WITH BORLAND C++)"<<endl;
+      cout<<"NOTE      : Gunakan (Username:Arif) dan (Password:2wisezx)"<<endl;
+   	cout<<"---------------------------------------------------------------"<<endl;
+   }
+}
